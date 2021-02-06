b498d7cf38a90e87c469aceb4aa875f2332ec232
vinhvn <vinhhnguyen@cmail.carleton.ca>
2021-02-06 12:51:15 -0500

    feat: :rocket: implemented twitch bot api

---
+++ src/twitch-bot.js
@@ -0,0 +1,101 @@
+"use strict";
+exports.__esModule = true;
+var tmi = require("tmi.js");
+var envs_json_1 = require("./envs.json");
+var fs = require("fs");
+// Main
+var queue = [];
+var votes = {};
+var timer = setInterval(function () {
+    queueMove();
+}, 5000);
+var onMessageHandler = function (target, context, msg, self) {
+    // don't listen to self
+    if (self || !currentTurn()) {
+        return;
+    }
+    // remove spaces
+    var move = msg.trim();
+    // message should only contain move
+    var splits = move.split(' ');
+    if (splits.length > 1) {
+        move = splits[0];
+    }
+    // check if move is valid
+    if (!validMove(move)) {
+        return;
+    }
+    // add move to queue
+    addMove(move);
+};
+var onConnectedHandler = function (address, port) {
+    console.log("> Connected to " + address + ":" + port);
+};
+var currentTurn = function () {
+    var result = JSON.parse(fs.readFileSync('./turn.txt', 'utf8'));
+    return result == "True";
+};
+var validMove = function (s) {
+    var regex = /^[a-h][1-8][a-h][1-8]$/g;
+    return regex.test(s);
+};
+var addMove = function (move) {
+    if (!Object.prototype.hasOwnProperty.call(votes, move)) {
+        votes[move] = 1;
+    }
+    else {
+        votes[move] += 1;
+    }
+};
+var queueMove = function () {
+    var max = 0;
+    var qMove = "";
+    // find max
+    for (var move in votes) {
+        if (votes[move] > max) {
+            max = votes[move];
+            qMove = move;
+        }
+    }
+    // push to q and clear it from votes
+    queue.push(qMove);
+    clearVote(qMove);
+    writeQueue();
+};
+var clearVote = function (move) {
+    if (Object.prototype.hasOwnProperty.call(votes, move)) {
+        delete votes[move];
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+var writeQueue = function () {
+    var data = formatQueue();
+    fs.writeFile('moves.txt', data, function (err) {
+        console.log(err);
+    });
+};
+var formatQueue = function () {
+    var output = "";
+    for (var _i = 0, queue_1 = queue; _i < queue_1.length; _i++) {
+        var move = queue_1[_i];
+        output += move + "\n";
+    }
+    return output;
+};
+// Bot
+var opts = {
+    identity: {
+        username: envs_json_1.twitch_username,
+        password: envs_json_1.twitch_token
+    },
+    channels: [
+        envs_json_1.twitch_channel,
+    ]
+};
+var client = new tmi.client(opts);
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+client.connect();
---
+++ src/twitch-bot.ts
@@ -0,0 +1,113 @@
+import * as tmi from 'tmi.js'
+import { twitch_username, twitch_token, twitch_channel }  from './envs.json'
+import * as fs from 'fs'
+
+// Main
+
+const queue: string[] = [];
+const votes: { [key: string]: number } = {};
+const timer = setInterval(() => {
+  queueMove()
+}, 5000)
+
+const onMessageHandler = (target: any, context: any, msg: string, self: any) => {
+  // don't listen to self
+  if (self || !currentTurn()) {
+    return;
+  }
+  // remove spaces
+  let move = msg.trim()
+  // message should only contain move
+  const splits = move.split(' ')
+  if (splits.length > 1) {
+    move = splits[0];
+  }
+  // check if move is valid
+  if (!validMove(move)) {
+    return;
+  }
+  // add move to queue
+  addMove(move)
+}
+
+const onConnectedHandler = (address: string, port: number) => {
+  console.log(`> Connected to ${address}:${port}`)
+}
+
+const currentTurn = () => {
+  const result = JSON.parse(fs.readFileSync('./turn.txt', 'utf8'))
+  return result == "True"
+}
+
+const validMove = (s: string) => {
+  const regex = /^[a-h][1-8][a-h][1-8]$/g
+  return regex.test(s)
+}
+
+const addMove = (move: string) => {
+  if (!Object.prototype.hasOwnProperty.call(votes, move)) {
+    votes[move] = 1
+  } else {
+    votes[move] += 1
+  }
+}
+
+const queueMove = () => {
+  let max = 0
+  let qMove = ""
+  // find max
+  for (const move in votes) {
+    if (votes[move] > max) {
+      max = votes[move]
+      qMove = move
+    }
+  }
+  // push to q and clear it from votes
+  queue.push(qMove)
+  clearVote(qMove)
+  writeQueue()
+}
+
+const clearVote = (move: string) => {
+  if (Object.prototype.hasOwnProperty.call(votes, move)) {
+    delete votes[move]
+    return true
+  } else {
+    return false
+  }
+}
+
+const writeQueue = () => {
+  const data = formatQueue()
+  fs.writeFile('moves.txt', data, (err) => {
+    console.log(err)
+  })
+}
+
+const formatQueue = () => {
+  let output = ""
+  for (const move of queue) {
+    output += `${move}\n`
+  }
+  return output
+}
+
+
+// Bot
+
+const opts = {
+  identity: {
+    username: twitch_username,
+    password: twitch_token,
+  },
+  channels: [
+    twitch_channel,
+  ]
+}
+
+const client = new tmi.client(opts)
+
+client.on('message', onMessageHandler)
+client.on('connected', onConnectedHandler)
+
+client.connect()
